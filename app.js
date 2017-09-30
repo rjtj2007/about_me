ded32b2ef0c718a24b1b6059e328d16541f4b734
rjtj2007 <teacherman1@gmail.com>
2017-09-30 14:42:53 -0700

    added my number guessing game

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
-// // 'use strict';
-// //
-// // var userPoints = 0;
-// //
-// // console.log('Hello Everybody!');
-// // alert('welcome to my guessing game');
-// //
-// // var user = prompt('what is your name?');
-// // alert('Hi ' + user + ' welcome to our game!');
-// //
-// // var answer = prompt('Question... Type yes or no.').toLowerCase();
-// //
-// // if(answer === 'yes' || answer === 'y'){
-// //   userPoints++;
-// // } else {
-// //   alert('answer to your question');
-// // }
-// //
-// // alert('you have ' + userPoints);
-//
-// 'use strict';
-//
+//collect user points in about_me game
 var userPoints = 0;
+console.log('I\'m awake!');
 
-// var user = prompt('Hi there. What\'s your name?');
-// alert('Welcome ' + user + ' let\'s play a game.');
-// console.log('User name is: ' + user);
+alert('Let\'s play a guessing game!!');
+//ask user for name
+var user = prompt('What\'s your name?');
+//require user name to continue
+while (!user) {
+  user = prompt('Sorry, I didn\'n catch that. What was your name??');
+}
+//user name collected, move forward into the game
+alert('Welcome ' + user + ' let\'s play a game.');
+console.log('User name is: ' + user);
 //
 // var name = prompt(user + ', is my name Ryan?').toLowerCase();
 // if (name === 'yes' || 'y') {
@@ -52,16 +39,16 @@ var userPoints = 0;
 //   console.log('Siblings question user stated: ' + siblings);
 // }
 //
-var home = prompt('How many States have I called HOME?');
-home = parseInt(home);
-if (home === 4) {
-  userPoints++;
-  alert('WOW, that\'s amazing! I have lived in Washington, Oregon, Colorado and Alaska. ' + userPoints + ' points');
-  console.log('home question user stated: ' + home);
-} else {
-  alert('Sorry, 4 is the correct answer. Washington, Oregon, Colorado and Alaska.');
-  console.log('Home question user stated: ' + home);
-}
+// var home = prompt('How many States have I called HOME?');
+// home = parseInt(home);
+// if (home === 4) {
+//   userPoints++;
+//   alert('WOW, that\'s amazing! I have lived in Washington, Oregon, Colorado and Alaska. ' + userPoints + ' points');
+//   console.log('home question user stated: ' + home);
+// } else {
+//   alert('Sorry, 4 is the correct answer. Washington, Oregon, Colorado and Alaska.');
+//   console.log('Home question user stated: ' + home);
+// }
 //
 // var cat = prompt('Very important question ' + user + '. Do I have a Cat?').toLowerCase();
 // if (cat === 'yes' || 'y') {
@@ -89,5 +76,28 @@ if (home === 4) {
 //   alert('Please answer with \'yes\' or \'no\'');
 //   console.log('hair question user stated: ' + hair);
 // }
-//
+
+//Question 6: number guessing game
+var i = 0;
+alert(user + ' can you guess my favorite Number?');
+while (i < 4) {
+  var favNumber = prompt('Hint: pick a number between 1 and 20, I\'ll give you 4 tries to guess.');
+  favNumber = parseInt(favNumber);
+  if (favNumber === 13) {
+    alert('You guessed it! Great job ' + user);
+    userPoints++;
+    i = 5;
+  } else if (favNumber > 13) {
+    alert('Sorry too high.');
+    i++;
+  } else {
+    alert('Oops, too low.');
+    i++;
+  }
+  if (i === 4 && favNumber !== 13) {
+    alert('Better luck next time ' + user + '.');
+  }
+}
+console.log(favNumber);
+
 // alert('Thanks for playing ' + user + '. You have ' + userPoints + ' total points. Hope you learned something about me.');
